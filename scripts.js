345ae9f3b5614385009f0d96602cf3d3203b8420
Elizabeth Sun <elizabeth.sun@berkeley.edu>
2023-04-16 15:21:55 -0700

    added dropdowns to navigation bar and updated community spotlight

--- scripts.js
+++ scripts.js
@@ -36,6 +36,18 @@ function topbar() {
   }
 }
 
+// Show/hide dropdown content on hover
+var dropdowns = document.getElementsByClassName("dropdown");
+for (var i = 0; i < dropdowns.length; i++) {
+  dropdowns[i].addEventListener("mouseover", function () {
+    this.getElementsByClassName("dropdown-content")[0].style.display = "block";
+  });
+  dropdowns[i].addEventListener("mouseout", function () {
+    this.getElementsByClassName("dropdown-content")[0].style.display = "none";
+  });
+}
+
+
 
 
 // var slideIndex = [1,1];
